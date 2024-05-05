cc598c65ac285d43d3ee624bc7ddca828610da15
sıla <sla@SILA>
2024-05-05 13:20:09 +0300

    Proje dosyası ekle.

---
+++ oyun2/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oyun2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] kelimeler = { "kulaklık", "yazılım", "kapı", "kemik", "bilgisayar", "köpük", "ramazan", "bayram" };
+            string secilenkelime;
+            int hak = 6;
+            Random rnd = new Random();
+            secilenkelime =kelimeler[rnd.Next(0,kelimeler.Length-1)];
+            char[] yertutucu = new char[secilenkelime.Length];
+            for (int i = 0; i < yertutucu.Length; i++)
+            {
+                yertutucu[i] = '-';
+                Console.Write('-');
+            }
+            Console.WriteLine();
+            while (true)
+            {
+                Console.WriteLine("Kalan hak:{0}", hak);
+                Console.WriteLine("Bir harf giriniz");
+                char harf =Convert.ToChar(Console.ReadLine());
+                bool a = false;
+                int kalankelime = 0;
+                for (int i = 0; i < secilenkelime.Length; i++)
+                {
+
+
+                    if (secilenkelime[i] == harf)
+                    {
+                        yertutucu[i] = harf;
+                        a = true;
+                    }
+                    else if (yertutucu[i] == '-')
+                    {
+                        kalankelime++;
+
+                    }
+                    Console.WriteLine(yertutucu[i]);
+                }
+                Console.WriteLine();
+                if (kalankelime == 0)
+                {
+                    Console.WriteLine("Tebrikler");
+                    break;
+                }
+                if (a == false)
+                {
+                    hak--;
+                }
+                  if (hak == 0)
+                {
+                    Console.WriteLine("Kaybettiniz");
+                    break;
+                }
+                Console.WriteLine();
+
+
+            }
+
+            Console.WriteLine("Seçtiğin Kelime:{0}", secilenkelime);
+            Console.ReadLine();
+
+
+        }
+    }
+}
---
+++ oyun2/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Bir bütünleştirilmiş koda ilişkin Genel Bilgiler aşağıdaki öznitelikler kümesiyle
+// denetlenir. Bütünleştirilmiş kod ile ilişkili bilgileri değiştirmek için
+// bu öznitelik değerlerini değiştirin.
+[assembly: AssemblyTitle("oyun2")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP")]
+[assembly: AssemblyProduct("oyun2")]
+[assembly: AssemblyCopyright("Copyright © HP 2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible özniteliğinin false olarak ayarlanması bu bütünleştirilmiş koddaki türleri
+// COM bileşenleri için görünmez yapar. Bu bütünleştirilmiş koddaki bir türe
+// erişmeniz gerekirse ComVisible özniteliğini o türde true olarak ayarlayın.
+[assembly: ComVisible(false)]
+
+// Bu proje COM'un kullanımına sunulursa, aşağıdaki GUID tür kitaplığının kimliği içindir
+[assembly: Guid("41f0ce2a-de4c-4782-9f2e-15c8fa7ea400")]
+
+// Bir derlemenin sürüm bilgileri aşağıdaki dört değerden oluşur:
+//
+//      Ana Sürüm
+//      İkincil Sürüm 
+//      Yapı Numarası
+//      Düzeltme
+//
+// Tüm değerleri belirtebilir veya varsayılan Derleme ve Düzeltme Numaralarını kullanmak için
+// aşağıda gösterildiği gibi '*' kullanabilirsiniz:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
